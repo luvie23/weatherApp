d41de963f56190adcff79d01715a6725dce97ec9
Luvie Rallos <luvierallos@gmail.com>
2022-11-09 17:57:26 +0800

    add search functionality

--- src/index.js
+++ src/index.js
@@ -9,26 +9,27 @@ let cloudsDOM = document.getElementById('clouds');
 let temperatureDOM = document.getElementById('temperature');
 
 
-let leftBoard = document.getElementById('leftBoard');
 
-function getWeather(){
-    // const response = await fetch('http://api.weatherstack.com/current?access_key=80a89d202f4cbfc3daa8e17233f2e1d2&query=makati', {mode: 'cors'});
-    // const gifData = await response.json();
-    // console.log(gifData);
-    // cityDOM.textContent = gifData.location.name;
-    // dateDOM.textContent = gifData.location.localtime;
-    // cloudImg.src = gifData.current.weather_icons[0];
-    // cloudsDOM.textContent = gifData.current.weather_descriptions[0];
-    // temperatureDOM.textContent = `${gifData.current.temperature}°C`
+async function getWeather(city){
+    const response = await fetch(`http://api.weatherstack.com/current?access_key=80a89d202f4cbfc3daa8e17233f2e1d2&query=${city}`, {mode: 'cors'});
+    const gifData = await response.json();
+    console.log(gifData);
+    cityDOM.textContent = gifData.location.name;
+    dateDOM.textContent = gifData.location.localtime;
 
-    cityDOM.textContent = 'Makati';
-    dateDOM.textContent = '2022-11-09 | 15:23';
+    cloudsDOM.textContent = gifData.current.weather_descriptions[0];
+    temperatureDOM.textContent = `${gifData.current.temperature}°C`
 
-    cloudsDOM.textContent = 'Partly cloudy';
-    temperatureDOM.textContent = `32°C`
+
 
   
     }
+
+document.getElementById('searchButton').addEventListener('click', function (){
+    const search = document.getElementById('search');
+
+    getWeather(search.value)
+})
 
 
 
