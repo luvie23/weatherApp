65d64e2642cba23ff0c31ff8a39bdead050f4ab8
Luvie Rallos <luvierallos@gmail.com>
2022-11-09 18:11:12 +0800

    add enter key function on search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './style.css';
 
 
-
-
+let countryDOM = document.getElementById('country');
 let cityDOM = document.getElementById('city');
 let dateDOM = document.getElementById('date');
 let cloudsDOM = document.getElementById('clouds');
@@ -12,26 +11,27 @@ let temperatureDOM = document.getElementById('temperature');
 
 async function getWeather(city){
     const response = await fetch(`http://api.weatherstack.com/current?access_key=80a89d202f4cbfc3daa8e17233f2e1d2&query=${city}`, {mode: 'cors'});
-    const gifData = await response.json();
-    console.log(gifData);
-    cityDOM.textContent = gifData.location.name;
-    dateDOM.textContent = gifData.location.localtime;
+    const weatherData = await response.json();
+    console.log(weatherData);
+    countryDOM.textContent = weatherData.location.country;
+    cityDOM.textContent = weatherData.location.name;
+    dateDOM.textContent = weatherData.location.localtime;
+    cloudsDOM.textContent = weatherData.current.weather_descriptions[0];
+    temperatureDOM.textContent = `${weatherData.current.temperature}°C`;
 
-    cloudsDOM.textContent = gifData.current.weather_descriptions[0];
-    temperatureDOM.textContent = `${gifData.current.temperature}°C`
-
-
-
-  
     }
 
 document.getElementById('searchButton').addEventListener('click', function (){
     const search = document.getElementById('search');
 
-    getWeather(search.value)
+    getWeather(search.value);
 })
 
-
+document.getElementById('search').addEventListener('keydown', function (e) {
+    if (e.code === 'Enter') {
+        getWeather(search.value)
+    }
+})
 
 
 getWeather();
